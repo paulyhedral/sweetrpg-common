0fb494a64fbc1ed1716df48ab9e58af338de023c
Paul Schifferer <paul@schifferers.net>
2021-09-13 20:35:37 -0700

    Update requires

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="sweetrpg-common",
-    install_requires=[
-        "Flask >= 2.0",
-    ],
+    install_requires=["marshmallow==3.13.0", "pymongo[tls]==3.12.0"],
     extras_require={},
 )
