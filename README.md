d71f4188eb61329ad9fb2b9b7a2482779927bbb4
paulyhedral <paulyhedral@users.noreply.github.com>
2021-09-17 03:48:52 +0000

    deploy: a4349797b4f0a96e57b6387824a959ad4bcf3e74

--- README.md
+++
@@ -1,3 +0,0 @@
-# Common
-
-[![Unit tests](https://github.com/sweetrpg/common/actions/workflows/python-tests.yml/badge.svg)](https://github.com/sweetrpg/common/actions/workflows/python-tests.yml)
--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
-setup(
-    name="sweetrpg-common",
-    install_requires=["marshmallow==3.13.0", "pymongo[tls]==3.12.0"],
-    extras_require={},
-)
