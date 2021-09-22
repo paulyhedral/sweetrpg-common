4c761f0199085f0317d7a28d3a16cea9fc908ebd
Paul Schifferer <paul@schifferers.net>
2021-09-21 21:13:23 -0700

    Update requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="sweetrpg-db",
-    install_requires=["Flask-PyMODM @ git+https://github.com/sweetrpg/flask-pymodm.git"],
+    install_requires=["Flask-PyMODM @ git+https://github.com/sweetrpg/flask-pymodm.git", "marshmallow"],
     extras_require={},
 )
--- src/sweetrpg_db/schema/base.py
+++ src/sweetrpg_db/schema/base.py
@@ -3,33 +3,33 @@ __author__ = "Paul Schifferer <dm@sweetrpg.com>"
 """Abstract classes for database schema classes.
 """
 
-# from marshmallow import Schema, fields, EXCLUDE
-# from marshmallow import pre_load
-# from datetime import datetime
+from marshmallow import Schema, fields, EXCLUDE
+from marshmallow import pre_load
+from datetime import datetime
 
 
-# class BaseDBSchema(Schema):
-#     """Base type for schema classes.
-#     Inherit from this type for database schema objects to gain ID and date conversion
-#     pre-load functionality, and to have ID and audit fields setup automatically.
-#     """
+class BaseDBSchema(Schema):
+    """Base type for schema classes.
+    Inherit from this type for database schema objects to gain ID and date conversion
+    pre-load functionality, and to have ID and audit fields setup automatically.
+    """
 
-#     class Meta:
-#         unknown = EXCLUDE
+    class Meta:
+        unknown = EXCLUDE
 
-#     @pre_load
-#     def handle_id(self, in_data, **kwargs):
-#         in_data["id"] = in_data.get("_id") or in_data.get("id")
-#         return in_data
+    @pre_load
+    def handle_id(self, in_data, **kwargs):
+        in_data["id"] = in_data.get("_id") or in_data.get("id")
+        return in_data
 
-#     @pre_load
-#     def handle_dates(self, in_data, **kwargs):
-#         for k in ["created_at", "updated_at", "deleted_at"]:
-#             if isinstance(in_data.get(k), datetime):
-#                 in_data[k] = in_data[k].isoformat()
-#         return in_data
+    @pre_load
+    def handle_dates(self, in_data, **kwargs):
+        for k in ["created_at", "updated_at", "deleted_at"]:
+            if isinstance(in_data.get(k), datetime):
+                in_data[k] = in_data[k].isoformat()
+        return in_data
 
-#     id = fields.Str()  # as_string=True, dump_only=True)
-#     created_at = fields.DateTime(required=True)
-#     updated_at = fields.DateTime(required=True)
-#     deleted_at = fields.DateTime(allow_none=True)
+    id = fields.Str()  # as_string=True, dump_only=True)
+    created_at = fields.DateTime(required=True)
+    updated_at = fields.DateTime(required=True)
+    deleted_at = fields.DateTime(allow_none=True)
