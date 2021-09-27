7e8536d5830e05ca7d62bc5e013c97fa01f9de56
Paul Schifferer <paul@schifferers.net>
2021-09-26 20:57:25 -0700

    Fix borked workflow. Fix setup.py dependencies.

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="sweetrpg-db",
-    install_requires=["Flask-PyMODM @ git+https://github.com/sweetrpg/flask-pymodm.git", "marshmallow"],
+    install_requires=["mongoengine", "marshmallow"],
     extras_require={},
 )
