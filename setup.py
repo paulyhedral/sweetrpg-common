e63ee30a6d95a5397afff1e2c78378c64952eb7a
Paul Schifferer <paul@schifferers.net>
2021-10-03 13:14:42 -0700

    Update dependencies

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="sweetrpg-db",
-    install_requires=["mongoengine", "marshmallow"],
+    install_requires=["mongoengine", "marshmallow==3.13"],
     extras_require={},
 )
