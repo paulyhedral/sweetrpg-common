08d159ae8e014586659d6a5ae2aec50f0d01be73
Paul Schifferer <paul@schifferers.net>
2022-03-09 20:26:34 -0800

    Update Python versions and some action versions.

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="sweetrpg-db",
-    install_requires=["mongoengine", "marshmallow==3.14.1", "sweetrpg-model-core", "dnspython<3.0.0", "PyMongo[srv]==4.0.2"],
+    install_requires=["mongoengine @ git+https://github.com/MongoEngine/mongoengine.git", "marshmallow==3.14.1", "sweetrpg-model-core", "dnspython<3.0.0", "PyMongo[srv]==4.0.2"],
     extras_require={},
 )
