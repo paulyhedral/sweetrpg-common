9df1c15ce40a344bd9c8d88548b7d2cec5160a2d
Paul Schifferer <paul@schifferers.net>
2023-01-28 18:47:31 -0800

    Fix publish

--- src/sweetrpg_db/__init__.py
+++ src/sweetrpg_db/__init__.py
@@ -10,5 +10,5 @@ __version__ = "0.0.59"
 __build__ = 0x000001
 __author_email__ = "dm@sweetrpg.com"
 __license__ = "MIT"
-__copyright__ = "Copyright 2021 SweetRPG"
+__copyright__ = "Copyright 2023 SweetRPG"
 __cake__ = "\u2728 \U0001f370 \u2728"
